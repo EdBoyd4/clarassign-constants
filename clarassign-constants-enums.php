8478b54118567bbb93dd278af79491d45e37f3c6
EdBoyd4 <tad.boyd@clariuminvestigations.com>
2026-07-21 08:08:14 -0700

    improve and enhance new case and assignment intake

--- clarassign-constants-enums.php
+++ clarassign-constants-enums.php
@@ -3,3 +3,15 @@ declare(strict_types=1);
 
 const ENUM_SIDE_PLAINTIFF = 'plaintiff';
 const ENUM_SIDE_DEFENSE = 'defense';
+
+const ENUM_CASE_PHASES = [
+    'inquiry', 
+    'pre-claim', 
+    'claim', 
+    'pre-litigation', 
+    'litigation', 
+    'pre-trial', 
+    'trial', 
+    'post-verdict', 
+    'administrative'
+];
